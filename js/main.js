28d3e39e0d47b7691bd0698e19e96daeaa1df9a2
Frederick Foxxx <fwhduffield@gmail.com>
2017-06-04 18:43:04 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
-console.log("Up and Running!");
+var cards = [{
+    rank: 'queen',
+    suit: 'hearts',
+    cardImage: 'images/queen-of-hearts.png'
+  },
+  {
+    rank: 'queen',
+    suit: 'diamonds',
+    cardImage: 'images/queen-of-diamonds.png'
+  },
+  {
+    rank: 'king',
+    suit: 'hearts',
+    cardImage: 'images/king-of-hearts.png'
+  },
+  {
+    rank: 'king',
+    suit: 'diamonds',
+    cardImage: 'images/king-of-diamonds.png'
+  }
+];
 
-var cards = ["queen", "queen", "king",  "king"];
 var cardsInPlay = [];
 
 var checkForMatch = function(){
@@ -11,14 +30,16 @@ var checkForMatch = function(){
   }
 };
 
-
 var flipCard = function(cardId){
-  console.log("user flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("user flipped " + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
   if (cardsInPlay.length === 2){
   checkForMatch();
-}
+  }
 };
+
 
 flipCard(0);
 flipCard(2);
