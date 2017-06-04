c5d185040f59faabd4b8523886114e58e97069fb
Frederick Foxxx <fwhduffield@gmail.com>
2017-06-04 10:32:09 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 console.log("Up and Running!");
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "queen", "king",  "king"];
+var cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("user flipped " + cardOne);
+console.log("user flipped " + cardTwo);
+
+if (cardsInPlay.length === 2){
+  if (cardsInPlay[0] === cardsInPlay[1]){
+    alert("You found a match!!!");
+  } else {
+    alert("Sorry, keep trying");
+  }
+}
