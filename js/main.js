02e91eb2fbf7b3b1a8e5984661892e367f874390
Frederick Foxxx <fwhduffield@gmail.com>
2017-06-11 19:26:54 +0100

    create game logic

--- js/main.js
+++ js/main.js
@@ -30,16 +30,28 @@ var checkForMatch = function(){
   }
 };
 
-var flipCard = function(cardId){
+var flipCard = function(){
+  var cardId = this.getAttribute('data-id');
   console.log("user flipped " + cards[cardId].rank);
   cardsInPlay.push(cards[cardId].rank);
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
+  this.setAttribute('src', cards[cardId].cardImage)
   if (cardsInPlay.length === 2){
   checkForMatch();
   }
 };
 
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+  for (var i = 0; i < cards.length; i++){
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+};
+
+createBoard();
